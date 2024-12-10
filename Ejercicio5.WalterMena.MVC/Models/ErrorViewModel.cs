50813929c1ce23fe5c60fc0b543143908d62ea0b
Walter <walthermena0799@gmail.com>
2024-12-10 10:52:34 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio5.WalterMena.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio5.WalterMena.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
